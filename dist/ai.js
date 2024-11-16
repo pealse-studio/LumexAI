57dcdf5f839e0916417a801fa09ece4b87f66c63
Mlad <109038115+Pealse@users.noreply.github.com>
2024-11-16 21:19:50 +0200

    Add files via upload

--- dist/ai.js
+++ dist/ai.js
@@ -20,25 +20,11 @@ if ((0, fs_1.existsSync)(dataPath)) {
         fuzzyWordSet = (0, fuzzyset_1.default)(words);
     }
     catch (error) {
-        consoleLog("LumexAI", error);
         initEmptyDatabase();
     }
 }
 else {
     initEmptyDatabase();
-}
-function consoleLog(title, text) {
-    const options = {
-        timeZone: "Europe/Kiev",
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-    };
-    const now = new Date().toLocaleString('uk-UA', options);
-    console.log(`- \x1b[38;5;146m[${now}]\x1b[38;5;15m (${title}) \x1b[38;5;187m${text}\x1b[0m`);
 }
 function initEmptyDatabase() {
     ngramDatabase = { ngrams: {}, wordCount: 0 };
